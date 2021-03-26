51b1b5afee61ee63789ddf31abe686700f5a62c9
Abdul karim moro <Abdul.moro4u@gmail.com>
2021-03-26 15:28:37 +0900

    Making orders and cancelling orders on the decentralized exchange

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -24,9 +24,10 @@ contract Exchange {
     address constant ETHER = address(0); // store Ether in tokens mapping with blank address
     mapping(address => mapping(address => uint256)) public tokens;
 
-    mapping(uint256 => _order) public orders; // mapping the adress for all orders
+    mapping(uint256 => _Order) public orders; // mapping the adress for all orders
     uint256 public orderCount;
     mapping(uint256 => bool) public orderCancelled; // mapping the address for =orders cancelled
+    mapping(uint256 => bool) public orderFilled;// mapping the address for orders filled
 
 
 
@@ -42,6 +43,27 @@ contract Exchange {
         uint256 amountGet,
         address tokenGive,
         uint256 amountGive,
+        uint256 timestamp
+    );
+
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+        );
+
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        address userFill,
         uint256 timestamp
     );
 
@@ -99,10 +121,9 @@ contract Exchange {
     }
 
     function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive)public{
-        orderCount = orderCount.add(1)  // counting the number of order ids
-        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);// now is the timestamp.trackingthe order
-        emit Order(orderCount,msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
-
+    orderCount = orderCount.add(1); // counting the number of orders using ids.
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
     }
 
     
@@ -115,6 +136,31 @@ contract Exchange {
     }
 
 
-    function 
+    function  fillOrder(uint256 _id) public{ // we need to swap balances.
+
+        require(_id > 0 && _id <= orderCount, 'Error, wrong id');// check wether we are filling a valid order.
+        require(!orderFilled[_id], 'Error, order already filled'); // we check whether the order is filled already
+        require(!orderCancelled[_id], 'Error, order already cancelled');// we check whether the order is cancelled already.
+        _Order storage _order = orders[_id];
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+        orderFilled[_order.id] = true;
+        
+
+
+
+    }
+
+    function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+    // Fee paid by the user that fills the order, a.k.a. msg.sender.
+    uint256 _feeAmount = _amountGet.mul(feePercent).div(100); // deducting the fee
+
+    tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount)); // the user gets tokens
+    tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+    tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+    tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive); //the user exchange with another tokens
+    tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+
+    emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
+  }
 
 }
